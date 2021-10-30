5fbd2004aed435c7bc7649c37e35b5c55b269cfa
Brian3647 <victoris3647@gmail.com>
2021-10-30 10:57:26 +0200

    fix(clippy): Fix all clippy lints (and add a space before every line of information in the content box to make it look better)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use termion::color;
 use std::process;
+use termion::color;
 
 mod api;
 mod content_box;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
-    let username = std::env::args().nth(1).expect("No username given. Exiting...");
+    let username = std::env::args()
+        .nth(1)
+        .expect("No username given. Exiting...");
 
     // -v : print version and exit
     if username.eq("-v") {
-        const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
         println!("octofetch v{}", VERSION);
         process::exit(1);
     }
@@ -23,17 +24,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let accent = color::Fg(color::White);
 
     // The fetch
-    let mut info = content_box::ContentBox{ pushed_lines: Vec::new(), longest_line: 0 };
+    let mut info = content_box::ContentBox {
+        pushed_lines: Vec::new(),
+        longest_line: 0,
+    };
 
-    info.push(format!("{}Username: {}{}"  , main, accent, user.login));
+    info.push(format!("{}Username: {}{}", main, accent, user.login));
     if user.bio != None {
-        info.push(format!("{}Bio: {}{}"  , main, accent, user.bio.unwrap()));
+        info.push(format!("{}Bio: {}{}", main, accent, user.bio.unwrap()));
     }
-    info.push(format!("{}Repos: {}{}"     , main, accent, user.public_repos));
-    info.push(format!("{}Gists: {}{}"     , main, accent, user.public_gists));
-    info.push(format!("{}Follower: {}{}"  , main, accent, user.followers));
-    info.push(format!("{}Following: {}{}" , main, accent, user.following));
-    info.push(format!("{}Url: {}{}"       , main, accent, user.html_url));
+    info.push(format!("{}Repos: {}{}", main, accent, user.public_repos));
+    info.push(format!("{}Gists: {}{}", main, accent, user.public_gists));
+    info.push(format!("{}Follower: {}{}", main, accent, user.followers));
+    info.push(format!("{}Following: {}{}", main, accent, user.following));
+    info.push(format!("{}Url: {}{}", main, accent, user.html_url));
 
     println!("{}", info.to_string());
 
