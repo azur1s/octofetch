0ff34d3e8bad612975a3db94d6acfa93b00b0be2
Natapat Samutpong <natapat.samutpong@gmail.com>
2021-12-16 10:12:01 +0700

    test commit

--- src/api.rs
+++ src/api.rs
@@ -6,10 +6,10 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct UserData {
-    pub login: String,
-    pub name: Option<String>,
-    pub bio: Option<String>,
-    pub public_repos: i64,
+  pub login: String,
+  pub name: Option<String>,
+  pub bio: Option<String>,
+  pub public_repos: i64,
     pub public_gists: i64,
     pub followers: i64,
     pub following: i64,
@@ -24,7 +24,7 @@ pub async fn get(username: String) -> Result<UserData, Box<dyn std::error::Error
     let url = format!("{}{}", GITHUB_ENDPOINT, username);
 
     // Get the body of the request
-  let client = reqwest::Client::new();
+    let client = reqwest::Client::new();
     let res = client
         .get(url)
         .header(USER_AGENT, "octofetch cli")
